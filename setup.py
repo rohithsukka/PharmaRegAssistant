1e7ca2c9473a2c6bd453258b470225ac9600a651
Rohith Sukka <rohithhsukka@gmail.com>
2025-12-11 23:02:02 +0530

    Rename package from medical_chatbot to pharma-reg-assistant

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="pharma-reg-assistant",
+    version="0.1.0",
+    author="Rohith Sukka",
+    author_email="rohithhsukka@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
